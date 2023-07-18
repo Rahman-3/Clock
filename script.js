efa6b39b280df24c47c58ebc901e506647cf2a4d
Rahman-3 <139842058+Rahman-3@users.noreply.github.com>
2023-07-18 11:23:48 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Corrected code
+let hr = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+function displayTime() {
+  let date = new Date();
+
+  // Getting hour, mins, secs from datea
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+
+  let hRotation = 30 * (hh % 12) + mm / 2;
+  let mRotation = 6 * mm;
+  let sRotation = 6 * ss;
+
+  hr.style.transform = `rotate(${hRotation}deg)`;
+  min.style.transform = `rotate(${mRotation}deg)`;
+  sec.style.transform = `rotate(${sRotation}deg)`;
+}
+
+setInterval(displayTime, 1000); // Call the displayTime function every second (1000 milliseconds)
